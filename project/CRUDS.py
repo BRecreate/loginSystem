a9dc86a6beee2338a02630d12bc5df22f282130d
Breno <breno@recria.ai>
2025-07-04 11:44:43 -0300

    preparação de backend  login

--- project/CRUDS.py
+++ project/CRUDS.py
@@ -1,10 +1,33 @@
 import os
+from dotenv import load_dotenv
+
 from supabase import create_client
+load_dotenv()
 
 url = os.getenv('SUPABASE_URL')
 key = os.getenv('SUPABASE_KEY')
 supabase = create_client(url, key)
 
-data = supabase.table("user").select("*").execute()
 
-print(data)
+def create_user(email, password):
+    response = supabase.auth.sign_up(
+    {
+        "email": email,
+        "password": password,
+    }
+)
+    return response
+
+
+def login_user(email, password):
+    response = supabase.auth.sign_in_with_password(
+        {
+            "email": email,
+            "password": password,
+        }
+    )
+    return response
+
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+login_user(email, password)
